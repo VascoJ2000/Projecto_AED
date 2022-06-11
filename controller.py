606419504233ffbbbe9ea10f1d0a0469cc29f390
offmtm <86376547+offmtm@users.noreply.github.com>
2022-06-11 18:37:58 +0100

    commit 2
    atualizei o view

--- controller.py
+++ controller.py
@@ -1,7 +1,10 @@
 import model
+import view
+
 
 def consultare_lugares():
     return
+
 
 def reservar_lugares(lugares):
     for lugares in model.lugares_sala:
@@ -13,9 +16,9 @@ def reservar_lugares(lugares):
 
 def __init__(self):
     pass
-         
 
-def valido_letra (letra):
+
+def valido_letra(letra):
     if str(letra) in model.filas:
         return True
 
@@ -29,128 +32,127 @@ def valido_numero(numero):
 
     else:
         if int(numero) > 14:
-            return False 
+            return False
 
-    
-def dicionario_res (self,v1,v2):
-    self.value=[self.chave ,'fileira:',v1, 'cadeira',v2]
-        
-        
 
-def dicionario_canc (self,v1,v2):
-    self.value=['fileira:',v1, 'cadeira',v2]
+def dicionario_res(self, v1, v2):
+    self.value = [self.chave, 'fileira:', v1, 'cadeira', v2]
+
+
+def dicionario_canc(self, v1, v2):
+    self.value = ['fileira:', v1, 'cadeira', v2]
     for self.x in self.dic.values():
         if self.value in self.x:
-            self.chave=self.x[0][0]
-    
-    
-def nome (self):
-    self.name=self.name.title()
-    self.name=self.name.strip()
-    self.chave=self.name
-    
-    
-def teatro (self,fileira,cadeira):   
-    self.f=fileira
-    self.c=cadeira
-    self.total=self.f*self.c
-    self.disponivel=self.total
-    self.dic={}
-        
-    self.matriz=[]
+            self.chave = self.x[0][0]
+
+
+def nome(self):
+    self.name = self.name.title()
+    self.name = self.name.strip()
+    self.chave = self.name
+
+
+def teatro(self, fileira, cadeira):
+    self.f = fileira
+    self.c = cadeira
+    self.total = self.f * self.c
+    self.disponivel = self.total
+    self.dic = {}
+
+    self.matriz = []
     for i in range(fileira):
-        self.fileira1=[]
+        self.fileira1 = []
         for i in range(cadeira):
             self.fileira1.append('Vago')
-        self.matriz.append(self.fileira1)    
-    
-   
-def reserva (self, letra, numero, escolhafil, escolhacad):
+        self.matriz.append(self.fileira1)
+
+
+def reserva(self, letra, numero, escolhafil, escolhacad):
     while True:
-        try:    
-            if  "Ocupado" in self.matriz [escolhafil-1] [escolhacad-1]:
+        try:
+            if "Ocupado" in self.matriz[escolhafil - 1][escolhacad - 1]:
                 print("\n Este local Já esta Ocupado, Operação invalida !!! \n")
                 break
         except IndexError:
-            print('valor fora do range fileira ou cadeira inexistente')        
-            self.fil=(input('Digite uma letra de uma Fileira existente: '))
-            escolhafil=self.valido(self.fil)
-            self.cad=(input('Digite um numero de uma Cadeira existente: '))
-            escolhacad=self.valido(self.cad)
-        else:   
-                self.matriz [escolhafil-1] [escolhacad-1] = "Ocupado"
-                self.disponivel-=1
-                self.dicionario_res(escolhafil, escolhacad)
-                self.dic[self.chave]=self.value
-                print("\n RESERVA EXECUTADA !! \n")
-                self.next = 1
-                break
-    
-   
-def cancela (self, escolhafil, escolhacad, letra, numero):
+            print('valor fora do range fileira ou cadeira inexistente')
+            self.fil = (input('Digite uma letra de uma Fileira existente: '))
+            escolhafil = self.valido(self.fil)
+            self.cad = (input('Digite um numero de uma Cadeira existente: '))
+            escolhacad = self.valido(self.cad)
+        else:
+            self.matriz[escolhafil - 1][escolhacad - 1] = "Ocupado"
+            self.disponivel -= 1
+            self.dicionario_res(escolhafil, escolhacad)
+            self.dic[self.chave] = self.value
+            print("\n RESERVA EXECUTADA !! \n")
+            self.next = 1
+            break
+
+
+def cancela(self, escolhafil, escolhacad, letra, numero):
     self.next = 0
     while True:
-        try: 
-            if "Vago" in self.matriz [escolhafil-1] [escolhacad-1]:
+        try:
+            if "Vago" in self.matriz[escolhafil - 1][escolhacad - 1]:
                 print("\n Este local já esta Vago, Operação invalida !!!")
                 break
         except IndexError:
-            print('valor fora do range fileira ou cadeira inexistente')        
-            self.fil=(input('Digite uma letra de uma Fileira existente: '))
-            escolhafil=self.valido(self.fil, letra)
-            self.cad=(input('Digite um numero de uma Cadeira existente: '))
-            escolhacad=self.valido(self.cad, numero)             
-        
+            print('valor fora do range fileira ou cadeira inexistente')
+            self.fil = (input('Digite uma letra de uma Fileira existente: '))
+            escolhafil = self.valido(self.fil, letra)
+            self.cad = (input('Digite um numero de uma Cadeira existente: '))
+            escolhacad = self.valido(self.cad, numero)
+
         else:
-            self.dicionario_canc(escolhafil,escolhacad)
-            print("Deseja cancelar a reserva de:",self.dic[self.chave]," S / N ? ")
-            self.apaga=(input(": "))
+            self.dicionario_canc(escolhafil, escolhacad)
+            print("Deseja cancelar a reserva de:", self.dic[self.chave], " S / N ? ")
+            self.apaga = (input(": "))
             if 'S' in (self.apaga.upper()):
-                self.matriz [escolhafil-1] [escolhacad-1] = "Vago"
-                self.disponivel+=1
+                self.matriz[escolhafil - 1][escolhacad - 1] = "Vago"
+                self.disponivel += 1
                 self.dic.pop(self.chave)
                 print("\n RESERVA CANCELADA !! \n")
                 self.proximo = 1
                 break
             elif 'N' in (self.apaga.upper()):
-                print (" \n Reserva Mantida !!!")
+                print(" \n Reserva Mantida !!!")
                 break
             else:
-                print ("\n Não Valido tente Novamente !!!")    
-                   
-    
+                print("\n Não Valido tente Novamente !!!")
+
+
 def busca(self, nm):
-    self.name=str(nm)
+    self.name = str(nm)
     self.nome()
     return self.dic.get(self.chave)
-    
-    
-def reset (self,reset):
+
+
+def reset(self, reset):
     if 'S' in (reset.upper()):
-        self.teatro(self.f,self.c)
-        print (" \n CONTROLE RESETADO !!!")
+        self.teatro(self.f, self.c)
+        print(" \n CONTROLE RESETADO !!!")
     elif 'N' in (reset.upper()):
-        print (" \n Reset Cancelado !!!")
+        print(" \n Reset Cancelado !!!")
     else:
-        print ("\n Não Valido tente Novamente !!!")
-    
-    
-def getProximo (self):
+        print("\n Não Valido tente Novamente !!!")
+
+
+def getProximo(self):
     return self.next
-    
-def getMatriz (self):
+
+
+def getMatriz(self):
     return self.matriz
-      
-        
-#parte Exibir a matriz com os lugares vagos ou ocupados
-   
-    def exibeMatriz(self):
+
+    # parte Exibir a matriz com os lugares vagos ou ocupados
+
+    def exibirMatriz(self):
         for fileiras in range(len(self.matriz)):
-            print("\nFILEIRA",fileiras+1)
+            print("\nFILEIRA", fileiras + 1)
             for cadeiras in range(len(self.matriz[0])):
-                print("Cadeira",cadeiras+1,'(',
-                self.matriz[fileiras][cadeiras],')',end="  /  ")
+                print("Cadeira", cadeiras + 1, '(',
+                      self.matriz[fileiras][cadeiras], ')', end="  /  ")
             print('\n')
-        
-        print('\nCapacitade Maxima: ',self.total,'Lugares')
-        print('Lugares Disponiveis: ',self.disponivel,'\n')
+
+        print('\nCapacitade Maxima: ', self.total, 'Lugares')
+        print('Lugares Disponiveis: ', self.disponivel, '\n')
--- view.py
+++ view.py
@@ -1,3 +1,5 @@
+import sys
+
 import model
 import controller
 
@@ -22,7 +24,7 @@ def main ():
 
     while True:
         try:
-            comandos = input().split(" ")
+            comandos = input().split(" ")   #separa os comandos em uma lista
         except EOFError:
             return
     
@@ -42,16 +44,41 @@ def main ():
             print(model.Palco)
             print("\n Escolha um lugar vago \n")
             input("Digite a letra da fila desejada: ")
-            if controller.valido_letra(comandos) == True:
+            if controller.valido_letra(comandos) == True:                      #verifica se a letra digitada é válida
                 input("Digite o numero do lugar desejado : ")
 
-        if comandos[0] == "ER": #ER -> Eliminar reserva
-           print()
+        if comandos[0] == "ER": #ER -> Eliminar reserva  de lugar na sala
+            print("\n Escolha um lugar ocupado \n")
+            input("Digite a letra da fila desejada: ")                                         #letra da fila
+            if controller.valido_letra(comandos) == True:                                    #valido_letra
+                input("Digite o numero do lugar desejado : ")                                #numero do lugar
+                if controller.valido_numero(comandos) == True:                             #valido_numero
+                    print("\n Reserva eliminada \n")                                #reserva eliminada
+                else:                                                        #reserva eliminada
+                    print("\n Valor invalido \n")
+
+
+
+
 
 
         if comandos[0] == "AR": #AR -> Alterar reserva
-          print()
+            print("\n Escolha um lugar ocupado \n")
+            input("Digite a letra da fila desejada: ")                                         #letra da fila
+            if controller.valido_letra(comandos) == True:                                    #valido_letra
+                input("Digite o numero do lugar desejado : ")                                #numero do lugar
+                if controller.valido_numero(comandos) == True:                             #valido_numero
+                    print("\n Reserva alterada \n")                                #reserva alterada
+                else:                                                        #reserva alterada
+                    print("\n Valor invalido \n")
+
+
 
 
         if comandos[0] == "CVB": #CVB -> Consultar valor da bilheteira
-            print(*valor_bilheteira , sep = ' , ')
+            print("\n Valor da bilheteira: {} \n".format(valor_bilheteira))
+
+
+        if comandos[0] == "sair": #sair -> sair do programa
+            print("\n Obrigado por usar o programa \n")
+            sys.exit()
